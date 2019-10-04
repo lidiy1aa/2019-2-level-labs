7ccd82e4e2495338c105d943ea7a0b3dce0a3258
lidiy1aa <lanskaya.vbp@gmail.com>
2019-10-04 23:04:10 +0300

    uploaded my lab work

--- lab_1/main.py
+++ lab_1/main.py
@@ -2,22 +2,98 @@
 Labour work #1
 Count frequencies dictionary by the given arbitrary text
 """
+document = open('текст.txt', 'r')
+test = document.read()
+
+prohibited_marks = (',', '.', '\n', ':', ';', '#', '@', '$', '^', '&', '*', '%', '~', '"', '\'')
 
 
-def calculate_frequences(text: str) -> dict:
+def calculate_frequences(text):
+    def calculate_frequences(text):
+        if text == None:
+            print('None given')
+            freq_dict = {}
+            return freq_dict
+        else:
+            if type(text) is int:
+                print('The text is integer')
+                freq_dict = {}
+                return freq_dict
+            elif len(text) == 0:
+                print('The text is empty')
+                freq_dict = {}
+                return freq_dict
+            else:
+                freq_dict = {}
+                low_text = text.lower()
+                text_split = low_text.split()
+                res = []
+                for word in text_split:
+                    if not word.isdigit() and word not in prohibited_marks:
+                        clear_word = ''
+                        for c in word:
+                            if c not in prohibited_marks and not c.isdigit():
+                                clear_word += c
+                        if clear_word is not '':
+                            res.append(clear_word)
+                for word in res:
+                    count = freq_dict.get(word, 0)
+                    freq_dict[word] = count + 1
+                frequency_list = freq_dict.keys()
+                for words in frequency_list:
+                    print(words, freq_dict[words])
+
+        return freq_dict
     """
     Calculates number of times each word appears in the text
     """
     pass
 
-def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
+
+def filter_stop_words(freq_dict, stop_words):
+    if stop_words is None or freq_dict is None or len(freq_dict) == 0:
+        print('Error')
+        new_frequency = {}
+        return new_frequency
+    elif len(stop_words) == 0:
+        print(freq_dict)
+        return freq_dict
+    else:
+        new_frequency = freq_dict.copy()
+        for stop_word in stop_words:
+            if isinstance(stop_word, str) and len(stop_word) > 0:
+                if new_frequency.get(stop_word):
+                    new_frequency.pop(stop_word)
+        for key in new_frequency.keys():
+            if key in stop_words or isinstance(key, int):
+                new_frequency.pop(key)
+                print(new_frequency)
+                return new_frequency
+    return new_frequency
     """
     Removes all stop words from the given frequencies dictionary
     """
     pass
 
-def get_top_n(frequencies: dict, top_n: int) -> tuple:
+
+def get_top_n(new_frequency, top_n):
+    if new_frequency == {} or top_n <= 0:
+        return ()
+    else:
+        sorted_list = sorted(new_frequency.items(), key=lambda i: (i[1], i[0]), reverse=True)
+        final = []
+        final = [element[0] for element in sorted_list]
+        if top_n > len(final):
+            return tuple(final)
+        elif len(final) >= top_n:
+            final_top = final[:top_n]
+            print(tuple(final_top))
+            return tuple(final_top)
+
     """
     Takes first N popular words
     """
     pass
+
+def close_file():
+    
