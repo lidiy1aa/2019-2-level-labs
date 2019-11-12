8459e14f5db491e718c23770d6f2b1f9360ee960
lidiy1aa <lanskaya.vbp@gmail.com>
2019-11-12 10:19:26 +0300

    corrected mistakes

--- lab_2/main.py
+++ lab_2/main.py
@@ -9,14 +9,14 @@ def generate_edit_matrix(num_rows: int, num_cols: int) -> list:
     return edit_matrix
 
 
-edit_matrix = generate_edit_matrix(7, 8)
+matrix = generate_edit_matrix(7, 8)
 
 
 def initialize_edit_matrix(edit_matrix, add_weight, remove_weight):
     new_edit = list(edit_matrix)
-    if new_edit is None or len(new_edit) == 0 or len(new_edit[0]) == 0:
+    if new_edit is None or not new_edit or not new_edit[0]:
         return new_edit
-    elif type(add_weight) is int and type(remove_weight) is int:
+    elif isinstance(add_weight, int) and isinstance(remove_weight, int):
         new_edit[0][0] = 0
         for i in range(len(new_edit)):
             if i != 0:
@@ -27,11 +27,11 @@ def initialize_edit_matrix(edit_matrix, add_weight, remove_weight):
     return new_edit
 
 
-edit_matrix = tuple(initialize_edit_matrix(tuple(edit_matrix), 1, 1))
+matrix_new = tuple(initialize_edit_matrix(tuple(matrix), 1, 1))
 
 
 def minimum_value(numbers):
-    value = int(min(numbers))
+    value = (min(numbers))
     return value
 
 
@@ -43,7 +43,7 @@ def fill_edit_matrix(edit_matrix: tuple,
                      target_word: str):
     new_edit = list(edit_matrix)
     if new_edit:
-        if type(add_weight) is int and type(remove_weight) is int and type(substitute_weight) is int and\
+        if isinstance(add_weight, int) and isinstance(remove_weight, int) and isinstance(substitute_weight, int) and\
                 original_word and target_word:
             for i in range(1, len(new_edit)):
                 for j in range(1, len(new_edit[i])):
@@ -58,7 +58,7 @@ def fill_edit_matrix(edit_matrix: tuple,
     return new_edit
 
 
-new_edit = fill_edit_matrix(tuple(edit_matrix), 1, 1, 2, 'length', 'kitchen')
+edit = fill_edit_matrix(tuple(matrix_new), 1, 1, 2, 'length', 'kitchen')
 
 
 def find_distance(original_word: str,
@@ -67,26 +67,26 @@ def find_distance(original_word: str,
                   remove_weight: int,
                   substitute_weight: int) -> int:
     if isinstance(original_word, str) is True and isinstance(target_word, str) is True and \
-            isinstance(remove_weight, int) is True and \
+        isinstance(remove_weight, int) is True and \
             isinstance(substitute_weight, int) is True and isinstance(add_weight, int) is True:
             num_cols, num_rows = len(original_word) + 1, len(target_word) + 1
             new_edit = tuple(initialize_edit_matrix(tuple(generate_edit_matrix(num_cols, num_rows)), add_weight,
                                                     remove_weight))
-            matrix = fill_edit_matrix(new_edit, add_weight, remove_weight, substitute_weight, original_word,
+            final_matrix = fill_edit_matrix(new_edit, add_weight, remove_weight, substitute_weight, original_word,
                                       target_word)
-            print(matrix[-1][-1])
-            return matrix[-1][-1]
+            print(final_matrix[-1][-1])
+            return final_matrix[-1][-1]
     else:
         return -1
 
 
-find_distance('length', 'kitchen', 1, 1, 2)
+final = find_distance('length', 'kitchen', 1, 1, 2)
 
 
 def save_to_csv(path_to_file: str):
     with open (path_to_file, 'w') as file:
         writer = csv.writer(file)
-        writer.writerows(new_edit)
+        writer.writerows(edit)
     return None
 
 
